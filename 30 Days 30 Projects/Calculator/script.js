b3d03765bf1c2460d4680b922f6f2c80cda997a4
shakeelahmad8451 <shakeelahmad8451@gmail.com>
2025-02-14 15:19:00 +0500

    Updated script.js and removed redundancy from the code

--- 30 Days 30 Projects/Calculator/script.js
+++ 30 Days 30 Projects/Calculator/script.js
@@ -1,4 +1,4 @@
-function handleClick(e){
+/* function handleClick(e){
 
 
     if(e.target.value === 'AC'){
@@ -26,3 +26,40 @@ function handleClick(e){
         document.getElementById('display').value+=e.target.value;
     }
 }
+ */
+
+// The above code has a lot of redundancy and not easy to understand so here is the updated version of the function
+// Now this code is much cleaner and easy to read
+function handleClick(e){
+
+    const display =  document.getElementById('display');
+    const clickedButtonValue = e.target.value;
+
+   // if(e.target.value === 'AC'){
+    if(clickedButtonValue === 'AC'){
+        //document.getElementById('display').value="";
+        display.value='';
+    }
+    else if(clickedButtonValue === 'DE'){
+        display.value = display.value.slice(0,-1)
+    }
+    else if(clickedButtonValue === '='){
+        let equation = display.value;
+        display.value = '';
+        let results;
+        try{
+            results = eval(equation);
+        }catch(error){
+            alert('Invalid Expression !!!');
+            display.value = ''
+            return;
+        }
+        console.log(results);
+        display.value = results;
+    }
+    else{
+        console.log(e.target.value);
+        display.value+=clickedButtonValue;
+    }
+}
+
