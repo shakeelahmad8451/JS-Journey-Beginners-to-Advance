267ffe2f1813ca93931d119e2df27b263e65eebb
shakeelahmad8451 <shakeelahmad8451@gmail.com>
2025-02-15 20:25:20 +0500

    created ToDo List App

---
+++ 30 Days 30 Projects/ToDo/script.js
@@ -0,0 +1,78 @@
+function popUpBlock(){
+    // on click of this button we need to show the popup menu and override the display:none to display:flex;
+    const popUpDiv = document.getElementById('popaddnewtask');
+    console.log(popUpDiv)
+    console.log(popUpDiv.classList)
+    popUpDiv.classList.add('display');
+
+    // and also blur the screen we need to access the overlay div and give it display of block
+    const overlay = document.getElementById('overlay');
+    console.log(overlay.classList);
+    overlay.classList.add('overlay-visible')
+
+
+}
+
+function handleChange(event){
+    console.log(event.target.value);
+    // access the input and set the value to the event.target.value
+    const inputTag = document.getElementById('taskText');
+    inputTag.value=event.target.value;
+
+}
+
+function addTask(){
+    const inputTag = document.getElementById('taskText');
+
+        // if there is not text then show alert
+        if(inputTag.value === ''){
+            alert('Task cannot be empty !!!!');
+            return;
+        }
+
+    // when user clicks add task we need to append the task in the task container and give it a class of task
+        const taskContainer = document.getElementById('taskcontainer');
+        
+        const div = document.createElement('div');
+        const p =  document.createElement('p');
+        const input = document.createElement('input');
+        const i = document.createElement('i');
+
+        // Now we need to get the text what the user have enetered in the input field
+        // and put it inside the input tag
+        input.innerText=inputTag.value;
+        console.log(input.innerText)
+        // and set the input value to null or empty string
+        inputTag.value='';
+
+        console.log(input); // at this point we have p tag
+
+        i.innerText='X';
+        p.innerText='7:00 PM'
+
+        // now we need to put all these p,input and i in the div
+        div.appendChild(p);
+        div.appendChild(input);
+        div.appendChild(i);
+
+        // now add classes accordingly to div and other
+        div.classList.add('task');
+        p.classList.add('time');
+
+       // now put this div we have made till now into the taskcontainer
+       taskContainer.appendChild(div); 
+
+       //now after adding the task we need to close the popup and remove the overlay class from the div
+
+       const overlay = document.getElementById('overlay');
+       overlay.classList.remove('overlay-visible');
+
+       // and close the popup menu
+       const popUpDiv = document.getElementById('popaddnewtask');
+       popUpDiv.classList.remove('display');
+
+
+
+
+
+}
